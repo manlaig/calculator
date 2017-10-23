f2f3ceb5ab1a26937e236b56b969e98729080325
Enkhmanlai Ganzorig <manlaig@users.noreply.github.com>
2017-10-22 23:34:22 -0700

    Changing colors and deleting unnecessary files

--- app/src/main/java/com/example/android/calculator/MainActivity.java
+++ app/src/main/java/com/example/android/calculator/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.Window;
 import android.widget.TextView;
 
 import static com.example.android.calculator.StorageActivity.words;
@@ -21,6 +22,7 @@ public class MainActivity extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
 
         text = (TextView) findViewById(R.id.text);
